23c231aefbd046c827c28f8e297a1d57d0cce930
Hello-Liu0618 <2463141541@qq.com>
2026-03-01 14:29:58 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,6 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -96,6 +97,7 @@ public class DishController {
      */
     @PutMapping("admin/dish")
     @ApiOperation("修改菜品")
+    @CacheEvict(cacheNames="setmealCache" , allEntries = true)
     public Result update(@RequestBody DishDTO dishDTO) {
         log.info("修改菜品: {}", dishDTO);
         dishService.updateWithFlavor(dishDTO);//函数内部标记缓存脏位
