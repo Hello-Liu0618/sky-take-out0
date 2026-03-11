40b347b4b1b4f0165315ef5539bbe4544c0cbc89
Hello-Liu0618 <2463141541@qq.com>
2026-03-11 16:24:02 +0800

    用户统计业务代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 
 public interface UserMapper {
@@ -30,4 +32,11 @@ public interface UserMapper {
      */
     @Select("select * from user where id = #{userId}")
     User getById(Long userId);
+
+    /**
+     * 根据map求用户总数
+     * @param map
+     * @return
+     */
+    Long sumByMap(Map map);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,9 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
     public ReportServiceImpl(OrderMapper orderMapper) {
         this.orderMapper = orderMapper;
@@ -66,5 +71,52 @@ public class ReportServiceImpl implements ReportService {
                 .build();
 
         return turnoverReportVO;
+    }
+
+    /**
+     * 统计指定日期区间内的用户数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //当前集合存储从begin到end的所有日期
+        List<LocalDate> dates = new ArrayList<>();
+        LocalDate date = begin;
+        while (!date.isAfter(end)) {
+            dates.add(date);
+            date = date.plusDays(1);
+        }
+        List<Long> totalUserList =  new ArrayList<>();
+        List<Long> newUserList =  new ArrayList<>();
+        dates.forEach(date1 -> {
+            //根据date日期查询对应的用户数据
+            LocalDateTime dateTimeMin = LocalDateTime.of(date1, LocalTime.MIN);
+            LocalDateTime dateTimeMax = LocalDateTime.of(date1, LocalTime.MAX);
+            Map map = new HashMap();
+            map.put("end", dateTimeMax);
+            Long userTotal = userMapper.sumByMap(map);
+            if( null == userTotal){
+                userTotal = 0L;
+            }
+            totalUserList.add(userTotal);
+            map.put("begin", dateTimeMin);
+            Long userNew = userMapper.sumByMap(map);
+            if( null == userNew){
+                userNew = 0L;
+            }
+            newUserList.add(userNew);
+        });
+
+        String dateList = StringUtils.join(dates, ",");
+        String totalUserListString = StringUtils.join(totalUserList, ",");
+        String newUserListString = StringUtils.join(newUserList, ",");
+
+        UserReportVO userReportVO = UserReportVO.builder()
+                .dateList(dateList)
+                .totalUserList(totalUserListString)
+                .newUserList(newUserListString)
+                .build();
+        return userReportVO;
     }
 }
